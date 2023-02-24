dc8481f26e22b40b8816fb19bd0bfd08fcb8fc7c
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-02-24 13:10:26 -0500

    setup for visual testing

---
+++ index.js
@@ -0,0 +1,18 @@
+import { CanvasBoard } from "./modules/canvas_board";
+import { Solver } from './modules/solver';
+
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+if (window.screen.width < 500) {
+    canvas.width = window.screen.width * .95;
+    canvas.height = window.screen.width * .96;
+
+    
+} else {
+    canvas.height = 600;
+    canvas.width = 600;
+}
+
+
+const board = new CanvasBoard()
